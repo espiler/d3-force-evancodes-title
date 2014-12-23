fa4bee8dcbe48545a738cdd6919dfdcccefeb10b
espiler <monkeysrawesome@gmail.com>
2014-12-23 11:35:08 -0800

    implements single post view

--- client/js/NewPostController.js
+++ client/js/NewPostController.js
@@ -2,15 +2,18 @@ angular.module('evancodes.newPost', [])
 
 
 .controller('NewPostController', function($scope, $rootScope, $location, $stateParams) {
-
+	$scope.url = window.location.href
+	$scope.data = 'datahere'
 	$scope.newPost = function() {
 		console.log("posting............")
 		var ref = new Firebase("https://brilliant-torch-8757.firebaseio.com/posts");
-		var postsRef = ref.child($scope.title.toLowerCase().split(' ').join("-"));
+		var url = $scope.title.toLowerCase().split(' ').join("-")
+		var postsRef = ref.child(url);
 		postsRef.push({
 		  title: $scope.title,
 		  content: $scope.content,
-		  datePosted: Date.now()
+		  datePosted: Date.now(),
+		  url: url
 		});
 	}
 
--- client/js/PostController.js
+++ client/js/PostController.js
@@ -1,6 +1,20 @@
 angular.module('evancodes.posts', [])
 
-
-.controller('PostController', function($scope, $rootScope, $location, $stateParams) {
-	$scope.data = '************POST DATA***************'
+.controller('PostController', function($scope, $rootScope, $location, $window) {
+	var url = window.location.href;
+	var postUrl = url.toString().substring(30);
+	var getSinglePost = function() {
+		var ref = new Firebase("https://brilliant-torch-8757.firebaseio.com/posts/" + postUrl);
+		ref.on("value", function(snapshot) {
+			if (snapshot.val() === null) { $scope.notFound = true; }
+			else {
+				var currentPost = snapshot.val();
+				$scope.post = currentPost[Object.keys(currentPost)[0]];
+				console.log($scope.post);
+				$('.snippetTitle').text($scope.post.title);
+				$('.snippetBody').html($scope.post.content);
+			}
+		})
+	}
+	getSinglePost();
 })
