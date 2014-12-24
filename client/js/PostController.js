92586571f34e6d0727024980a5aad0b055fd100f
espiler <monkeysrawesome@gmail.com>
2014-12-23 18:00:53 -0800

    modifies URL substring for heroku app

--- client/js/PostController.js
+++ client/js/PostController.js
@@ -2,7 +2,7 @@ angular.module('evancodes.posts', [])
 
 .controller('PostController', function($scope, $rootScope, $location, $window) {
 	var url = window.location.href;
-	var postUrl = url.toString().substring(30);
+	var postUrl = url.toString().substring(39);
 	var getSinglePost = function() {
 		var ref = new Firebase("https://brilliant-torch-8757.firebaseio.com/posts/" + postUrl);
 		ref.on("value", function(snapshot) {
