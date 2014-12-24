2a5c36668d5bcf6eb7277c2532049ea01a0842f8
espiler <monkeysrawesome@gmail.com>
2014-12-23 17:13:04 -0800

    fixes port for heroku

--- server/index.js
+++ server/index.js
@@ -5,16 +5,14 @@ var morgan  = require('morgan');
 
 var app = express();
 
-var port = 8000;
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/..'));
 
 
-app.listen(port);
-console.log("Listening on localhost: " + port)
+app.listen(process.env.port || 8000);
+console.log("Listening on " + 8000)
 
 app.get('/', function(req, res) {
 })
